4356b186f6fbbd3203a5c4e623592308ebae7610
sachin <sachinksingh59@gmail.com>
2024-09-20 00:45:14 +0530

    github data added

--- index.js
+++ index.js
@@ -4,6 +4,41 @@ const app = express()
 
 const PORT = process.env.PORT
 
+const githubData = {
+  "login": "Adityasingh9958",
+  "id": 105357367,
+  "node_id": "U_kgDOBkegNw",
+  "avatar_url": "https://avatars.githubusercontent.com/u/105357367?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/Adityasingh9958",
+  "html_url": "https://github.com/Adityasingh9958",
+  "followers_url": "https://api.github.com/users/Adityasingh9958/followers",
+  "following_url": "https://api.github.com/users/Adityasingh9958/following{/other_user}",
+  "gists_url": "https://api.github.com/users/Adityasingh9958/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/Adityasingh9958/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/Adityasingh9958/subscriptions",
+  "organizations_url": "https://api.github.com/users/Adityasingh9958/orgs",
+  "repos_url": "https://api.github.com/users/Adityasingh9958/repos",
+  "events_url": "https://api.github.com/users/Adityasingh9958/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/Adityasingh9958/received_events",
+  "type": "User",
+  "site_admin": false,
+  "name": "Aditya Singh",
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": null,
+  "twitter_username": "SinghAditya2511",
+  "public_repos": 5,
+  "public_gists": 0,
+  "followers": 0,
+  "following": 0,
+  "created_at": "2022-05-11T10:34:01Z",
+  "updated_at": "2024-09-18T18:03:02Z"
+}
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -18,6 +53,10 @@ app.get('/login',(req , res)=>{
 
 app.get('/youtube',(req , res)=>{
     res.send("<h2>Chai aur code</h2>")
+})
+
+app.get('/github',(req , res)=>{
+  res.json(githubData)
 })
 
 app.listen(process.env.PORT, () => {
